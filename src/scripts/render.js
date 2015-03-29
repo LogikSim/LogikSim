083b7727e9b49528352d0739fa2436b042f56bf6
Christian Brugger <brugger.chr@gmail.com>
2015-03-30 00:05:13 +0200

    Mapping data to real objects

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -10,7 +10,8 @@ var scene_data = {
     items: [
         { type: "and", x: 5, y: 5 },
         { type: "or", x: 5, y: 8 },
-        { type: "xor", x: 10, y: 6 }
+        { type: "xor", x: 10, y: 6 },
+        { type: "interconnect", tree: [(7,6), [(10, 6)]]}
     ]
 };
 
@@ -67,50 +68,85 @@ scene.append("rect")
 // logicitems
 //
 
-// data type to svg types/classes
-var type_map = {
-    "and": {
-        types: "logic baseitem and",
-        text: "&"
-    },
-    "or": {
-        types: "logic baseitem or",
-        text: "≥1"
-    },
-    "xor": {
-        types: "logic baseitem xor",
-        text: "=1"
-    },
+function Item(data) {
+    this.types = "item";
+    this.x = data.x || 0;
+    this.y = data.y || 0;
 }
 
+function Logic(data) {
+    Item.call(this, data);
 
-// create all logic items
+    this.types += " logic";
+}
+
+function AndItem(data) {
+    Logic.call(this, data);
+
+    this.types += " baseitem and";
+    this.text = "&";
+}
+
+function OrItem(data) {
+    Logic.call(this, data);
+
+    this.types += " baseitem and";
+    this.text = "≥1";
+}
+
+function XorItem(data) {
+    Logic.call(this, data);
+
+    this.types += " baseitem and";
+    this.text = "=1";
+}
+
+function Interconnect(data) {
+    Item.call(this, data);
+
+    this.types += " interconnect";
+}
+
+var type_map = {
+    and: AndItem,
+    or: OrItem,
+    xor: XorItem,
+    interconnect: Interconnect,
+}
+
+items = [];
+scene_data.items.forEach(function (item) {
+    items.push(new type_map[item.type](item));
+});
+    
+// create all items
 var logicitems = scene.selectAll("g")
-    .data(scene_data.items)
+    .data(items)
     .enter().append("g")
     .attr("transform", function (d, i) {
         return "translate(" +
             to_grid(d.x) + "," + to_grid(d.y) + ")";
     })
     .attr("class", function (d, i) {
-        return type_map[d.type].types;
+        return d.types;
     });
-
+    
+    
 
 
 // create text items
-
+    
 var baseitems = scene.selectAll("g.baseitem");
-
+    
 var overlapp = 0.37;
-
+    
 // rect
 baseitems.append("rect")
     .attr("class", "logic")
     .attr("y", to_grid(-overlapp))
     .attr("width", to_grid(2))
     .attr("height", to_grid(2 + 2 * overlapp));
-
+    
 //input connector
 for (var i = 0; i < 3; ++i) {
     baseitems.append("line")
@@ -120,7 +156,7 @@ for (var i = 0; i < 3; ++i) {
         .attr("y1", to_grid(i))
         .attr("y2", to_grid(i));
 }
-
+    
 // output connector
 baseitems.append("line")
     .attr("class", "logic")
@@ -128,13 +164,15 @@ baseitems.append("line")
     .attr("x2", to_grid(2.5))
     .attr("y1", to_grid(1))
     .attr("y2", to_grid(1));
-
+    
 // label
 baseitems.append("text")
     .attr("class", "logic")
     .attr("x", to_grid(1))
     .attr("y", to_grid(1))
     .text(function (d, i) {
-        return type_map[d.type].text;
+        return d.text;
     });
-
+    
+    
+// create interconnect items
