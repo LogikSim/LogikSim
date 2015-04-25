7ca5f2eaf3a6847b3adf1d4860633621560d2014
Stefan Hacker <dd0t@users.sourceforge.net>
2015-04-25 13:04:34 +0200

    Add "serve" gulp task to replace serve.bat.
    This task provides a local webserver for testing the
    application. Having it as a gulp task should give us
    more flexibility in the future.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var jshint = require('gulp-jshint');
 var gulp = require('gulp-help')(require('gulp'));
 var karma = require('karma').server;
+var webserver = require('gulp-webserver');
 
 gulp.task('test', 'Run test once and exit', function (done) {
   karma.start({
@@ -15,9 +16,19 @@ gulp.task('tdd', 'Watch for file changes and re-run tests on each change', funct
   }, done);
 });
 
-gulp.task('lint', 'Lint source files and exit', function (done) {
+gulp.task('lint', 'Lint source files and exit', function () {
   return gulp.src(['src/**/*.js', '!src/vendor/**'])
-     .pipe(jshint())
-     .pipe(jshint.reporter('default'))
-     .pipe(jshint.reporter('fail'));
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('serve', 'Starts a server and opens the app in the browser', function() {
+  return gulp.src('src')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
